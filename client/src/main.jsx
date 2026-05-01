6e5500d6d918757e387fae4f3467426e9abcebc8
CianFonseca18 <cianfonseca99@gmail.com>
2026-05-01 21:13:32 +0530

    Copied axios fix from downloads folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import axios from 'axios';
+axios.defaults.baseURL = 'https://fs-main.onrender.com';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
